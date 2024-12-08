184c9f705ae9dd20518019c8d9c09e4d0c7fdbd1
Nerd <hndppt@outlook.com>
2024-12-09 00:16:19 +0800

    fix: typo fix

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import "./globals.css";
 
+import React, { useEffect } from "react";
 import Credits from "@/components/Credits";
 import Footer from "@/components/Footer";
 import BladeRunner from "@/components/HomePage/BladeRunner";
 import _ from "lodash/fp";
-import React from "react";
 
-const inventory = () => {
+const Inventory = () => {
+    useEffect(() => {
+        setTimeout(() => {
+            console.log(
+                "这么想要知道这个页面的秘密, 不如直接看源码, 然后给我一个 star...",
+                "https://github.com/1216892614/diceshock",
+                "小さな星を摘んだなら, あなたは小さな幸せを手に入れる.",
+                "大きな星を摘んだなら, あなたは大きな富を手に入れる.",
+                "その両方を摘んだなら, あなたは永遠の願いを手に入れる!"
+            );
+        }, 20 * 1000);
+    }, []);
+
     return (
         <>
             <main>
@@ -75,4 +89,4 @@ const inventory = () => {
     );
 };
 
-export default inventory;
+export default Inventory;
