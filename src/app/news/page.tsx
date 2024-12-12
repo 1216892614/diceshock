20c0b31a1e4ad2ff6e166ab23dbd7d2285adbee7
Nerd <hndppt@outlook.com>
2024-12-12 23:29:43 +0800

    fix(news): fix

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -2,22 +2,23 @@
 
 import React from "react";
 import BladeRunner from "@/components/HomePage/BladeRunner";
+import Credits from "@/components/Credits";
 
 const Inventory = () => {
     return (
         <>
-            <main className="dont_disable_this_QAQ w-full h-[calc(100vh-5rem)] max-h-[calc(100vh-5rem)] overflow-x-hidden overflow-y-auto">
-                <BladeRunner
-                    texts={[
-                        ["暂时这里没什么新消息欸..."],
-                        ["无所谓了, 反正没开业, 没人会进来."],
-                        ["那你在读什么?"],
-                        ["居然还要向下看, ", "真的没什么可看的了."],
-                        ["既然你还要看,"],
-                        ["就看看我们的演职人员表吧..."],
-                    ]}
-                />
-            </main>
+            <BladeRunner
+                texts={[
+                    ["暂时这里没什么新消息欸..."],
+                    ["无所谓了, 反正没开业, 没人会进来."],
+                    ["那你在读什么?"],
+                    ["居然还要向下看, ", "真的没什么可看的了."],
+                    ["既然你还要看,"],
+                    ["就再看看我们的演职人员表吧..."],
+                ]}
+            />
+
+            <Credits />
         </>
     );
 };
