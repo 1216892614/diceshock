d9c49297f32648a8b2c720ba0c7301153730c9ae
Nerd <hndppt@outlook.com>
2024-12-29 03:23:10 +0800

    fix(LoadingImg): let img loading

--- src/components/GameList/LoadingImg.tsx
+++ src/components/GameList/LoadingImg.tsx
@@ -1,15 +1,14 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
-const LoadingImg: React.FC<React.ComponentProps<typeof Image>> = ({
-    ...props
-}) => {
+const LoadingImg: React.FC<React.ComponentProps<typeof Image>> = (props) => {
     const [isLoading, loading] = useState<boolean>(true);
-
-    if (isLoading) return <span className={`skeleton ${props.className}`} />;
 
     return (
         <Image
+            className={
+                isLoading ? `skeleton ${props.className}` : props.className
+            }
             {...props}
             alt={props.alt}
             onLoadingComplete={() => loading(false)}
